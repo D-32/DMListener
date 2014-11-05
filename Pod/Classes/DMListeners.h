e884d13bb0a4817a716c7a45a1f95d306d0ec770
Dylan Marriott <info@d-32.com>
2014-11-04 19:45:59 -0500

    Added documentation

--- Pod/Classes/DMListeners.h
+++ Pod/Classes/DMListeners.h
@@ -10,10 +10,31 @@
 
 typedef void(^NotifyBlock)(id obj);
 
+/**
+ *  DMListeners manages the list of listeners and provides methods for adding new listeners and notifying them.
+ *  Deallocated listeners will be automatically removed.
+ */
 @interface DMListeners : NSObject
 
+/**
+ *  Add a new listener
+ *
+ *  @param listener listener to add
+ */
 - (void)addListener:(id)listener;
+
+/**
+ *  Notify all listeners. The notifyBlock will be called for each still allocated listener.
+ *
+ *  @param notifyBlock block that will be called for each listener
+ */
 - (void)notifyListeners:(NotifyBlock)notifyBlock;
+
+/**
+ *  Returns all still allocated listeners.
+ *
+ *  @return all still allocated listeners
+ */
 - (NSArray *)listeners;
 
 @end
